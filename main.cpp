900a22514a2d767f4fe27f1e13ea61e19850a6be
Jibril Aïssaoui <jibril57000@gmail.com>
2021-07-09 20:53:21 +0200

    la vache

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+    
+    
+    cout << "        \\   ^__^"<<endl;
+    cout << "         \\  (oo)\\_______" << endl;
+    cout << "           (__)\\       )\\ /" << endl;
+    cout << "                ||----w |" << endl;
+    cout << "                ||     ||" <<endl;
+
+}
