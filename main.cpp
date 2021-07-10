3e1f2dee56275fd2710688ccc4cb9be071f81839
Jibril Aïssaoui <jibril57000@gmail.com>
2021-07-10 17:47:20 +0200

    nouveau animaux

--- main.cpp
+++ main.cpp
@@ -19,8 +19,66 @@ vector<string> split(string m){
 }
 
 
+void animal(string animal){
+    cout << endl;
+    if(animal == "cow"){
+        cout << "        \\   ^__^"<<endl;
+        cout << "         \\  (oo)\\_______" << endl;
+        cout << "           (__)\\       )\\ /" << endl;
+        cout << "                ||----w |" << endl;
+        cout << "                ||     ||" <<endl;
+    } else if(animal == "cat"){
+        cout << "  \\"<<endl;
+        cout << "   \\"<<endl;
+        cout << "   |\\---/|"<<endl;
+        cout << "   |(._.) |"<<endl;
+        cout << "    \\_`_/-..----."<<endl;
+        cout << " ___/ `   ' ,""+  \\ "<<endl;
+        cout << "(__...'   _\\    |`.___.';" << endl;
+        cout << "  (_,...'(_,.`__)/'.....+" << endl;
+    } else if (animal == "snake"){
+        cout << "  \\"<<endl;
+        cout << "   \\"<<endl;
+        cout << "    \\"<<endl;
+        cout << "     \\"<<endl;
+        cout << "           /^\\/^\\"<<endl;
+        cout << "         _|__|  O|"<<endl;
+        cout << "\\/     /~     \\_/ \\"<<endl;
+        cout << " \\____|__________/  \\"<<endl;
+        cout << "        \\_______      \\"<<endl;
+        cout << "                `\\     \\                 \\"<<endl;
+        cout << "                  |     |                  \\" << endl;
+        cout << "                 /      /                    \\" << endl;
+        cout << "                /     /                       \\\\" << endl;
+        cout << "              /      /                         \\ \\" << endl;
+        cout << "              /      /                         \\ \\"<< endl;
+        cout << "             /     /                            \\  \\" << endl;
+        cout << "           /     /             _----_            \\   \\" << endl;
+        cout << "          /     /           _-~      ~-_         |   |" << endl;
+        cout << "         (      (        _-~    _--_    ~-_     _/   |" << endl;
+        cout << "          \\      ~-____-~    _-~    ~-_    ~-_-~    /" << endl;
+        cout << "            ~-_           _-~          ~-_       _-~" << endl;
+        cout << "               ~--______-~                ~-___-~" << endl;
+
+        
+
+    } else if (animal == "dear"){
+        cout << "  \\"<<endl;
+        cout << "   \\"<<endl;
+        cout << "(             )" << endl;
+        cout << " `--(_   _)--'" << endl;
+        cout << "      Y-Y" << endl;
+        cout << "     /@@ \\" << endl;
+        cout << "    /     \\" << endl;
+        cout << "    `--'.  \\             ," << endl;
+        cout << "        |   `.__________/)" << endl;
+
+    }
+}
+
+
 int main(int argc, char *argv[]){
-    string message = argv[1];
+    string message = argv[2];
 
     vector<string> v = split(message);
     for (int i =0; i < v.size(); i++){
@@ -31,17 +89,15 @@ int main(int argc, char *argv[]){
         }
         
     }
-    cout << endl;
 
-    cout << "        \\   ^__^"<<endl;
-    cout << "         \\  (oo)\\_______" << endl;
-    cout << "           (__)\\       )\\ /" << endl;
-    cout << "                ||----w |" << endl;
-    cout << "                ||     ||" <<endl;
+
+    string animaltype = argv[1];
+    animal(animaltype);
 
 
 
 }
+
 
 
 
